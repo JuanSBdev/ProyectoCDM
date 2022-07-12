502e178045811025192a59d4f314a434c02082fc
Juan SB <juansb.dev@gmail.com>
2022-07-11 21:08:59 -0300

    contactate

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,10 +1,25 @@
 <template>
-    <div class="container">
-        <img src="https://cezannehr.com/wp-content/uploads/2019/11/Cezanne_Hr_Support_Team_Vector.png">
+    <div class="container row d-flex">
+        <img class="d-flex col-s-12" src="https://cezannehr.com/wp-content/uploads/2019/11/Cezanne_Hr_Support_Team_Vector.png">
+        <h1 class="col-s-12" >Regístrate para contactarte con nosotros!</h1>
     </div>
 </template>
 <style scoped>
-.container img{
+.container{
     position: absolute;
+    width: 100%;
+
+    justify-content: center;
+}
+.container img{
+    position: relative;
+    width: 60%;
+    margin-top: 10%;
+
+}
+.container h1{
+    color: aliceblue;
+    display: flex;
+    justify-content: center;
 }
 </style>
