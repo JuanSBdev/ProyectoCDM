d820681bc2e04c4c1fe70b5b724f5426811ad9b8
Juan SB <juansb.dev@gmail.com>
2022-07-11 22:19:32 -0300

    mejora

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import  {BootstrapVue3, } from 'bootstrap-vue-3'
+import  {BootstrapVue3,  } from 'bootstrap-vue-3'
+import { BootstrapIconsPlugin, } from 'bootstrap-icons-vue'
 import AOS from 'aos'
 const app = createApp(App)
 
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-import { BootstrapIconsPlugin } from 'bootstrap-icons-vue'
 app.use(BootstrapVue3)
 app.use(router)  
 app.use(BootstrapIconsPlugin)
+
 AOS.init();
 //const app = createApp(App)
 
--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,14 +1,19 @@
+<script setup>
+function succes(){
+    console.log("yeia")
+}
+</script>
 <template>
-    <div class="container row d-flex">
+    <div class="container row d-flex mx-5">
         <img class="d-flex col-s-12" src="https://cezannehr.com/wp-content/uploads/2019/11/Cezanne_Hr_Support_Team_Vector.png">
         <h1 class="col-s-12" >Regístrate para contactarte con nosotros!</h1>
-    </div>
+<b-button pill variant="success" class="col-2 mt-3 py-2" @click="success">Registrarme</b-button>
+     </div>
 </template>
 <style scoped>
 .container{
     position: absolute;
     width: 100%;
-
     justify-content: center;
 }
 .container img{
@@ -21,5 +26,8 @@
     color: aliceblue;
     display: flex;
     justify-content: center;
+}
+button{
+    position: relative;
 }
 </style>
