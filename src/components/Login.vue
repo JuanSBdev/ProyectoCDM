f3d55ddf30a260146dc3bc4811d39b8f65f1e9f2
Juan SB <juansb.dev@gmail.com>
2022-07-13 22:11:55 -0300

    contacto

---
+++ src/components/Login.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="abajo-contacto">
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <h1>Log-In</h1>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        >
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Pass:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="****"
+
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    
+  </div>
+  </template>
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: []
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+<style>
+.abajo-contacto{
+    z-index: 2;
+    padding-left: 8%;
+    display: flex;
+    position: absolute;
+    margin-top: 60rem;
+    width: 50%;
+    justify-content: center;
+}
+.abajo-contacto h1{
+        margin-left: -8%;
+padding-bottom: 5%;
+}
+</style>
--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Login from "../components/Login.vue"
 
 function registrarme(){
     console.log("quiere registrarse")
@@ -10,75 +11,12 @@ function registrarme(){
         <h1 class="col-s-12" >Regístrate para contactarte con nosotros!</h1>
 <!--  --><b-button pill variant="success" class=" col-2 mt-3 py-2" @click="registrarme"  >Registrarme</b-button>
      </div>
-      <div class="abajo-contacto">
-          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <h1>Log-In</h1>
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          placeholder="Enter email"
-          required
-        >
-        </b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-2" label="Your Pass:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.password"
-          placeholder="****"
-
-          required
-        ></b-form-input>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    
-  </div>
+     <div class="abajo">
+       <Login></Login>
+       <img src="https://indyme.com/wp-content/uploads/2020/11/lock-icon.png" alt="">
+     </div>
 </template>
-<script>
-  export default {
-    data() {
-      return {
-        form: {
-          email: '',
-          name: '',
-          food: null,
-          checked: []
-        },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
-      }
-    },
-    methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
-    }
-  }
-</script>
+
 <style scoped>
 .container{
     position: absolute;
@@ -97,19 +35,10 @@ function registrarme(){
     display: flex;
     justify-content: center;
 }
-button{
-    position: relative;
-}
-.abajo-contacto{
-    padding-left: 8%;
-    display: flex;
-    position: absolute;
-    margin-top: 50rem;
-    width: 50%;
-    justify-content: center;
-}
-.abajo-contacto h1{
-        margin-left: -8%;
-padding-bottom: 5%;
+.abajo img{
+position: absolute;
+padding-top: 60rem;
+margin-left: 60%;
+width: 20%;
 }
 </style>
