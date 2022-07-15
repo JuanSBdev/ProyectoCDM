9ae272904027525c06ba3c4b98cb20830203670a
Juan SB <juansb.dev@gmail.com>
2022-07-15 19:15:00 -0300

    asd

--- src/components/Historia.vue
+++ src/components/Historia.vue
@@ -9,14 +9,14 @@ defineExpose({ fut });
 </script>
 <template>
 <div class="container-historia row d-flex" @mouseover="fut">
-    <div class="izquierda col-6">
+    <div class="izquierda col-8 py-2">
         <h1  class="" >¿Como Funciona la app?</h1>
         <p class="">
             Cada vez que veas SYMBOLO este codigo QR puedes escanearlo desde tu camara y te mostrará información sobre el lugar en donde te encuentres,
             ya sea precios, historias, eventos que habrá, reseñas de otros usuarios e incluso beneficios en muchos lugares :) 
         </p>
     </div>
-    <div class="derecha col-4">
+    <div class="derecha col-3">
         <img   src="https://icones.pro/wp-content/uploads/2021/04/icone-de-telephone-portable-violet.png" alt="">
     </div>
 </div>
